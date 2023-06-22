298ca8b46d7f398d9c3dea4019468a0f53b40886
yukip-chottodake <yukip@chottodake.dev>
2023-06-22 19:01:51 +0000

    sh change & use tmp

--- init-admin.sh
+++ init-admin.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+set -v
+
 # set project
 gcloud config set project dev-chottodake-admin
 
-# api & service
+
+# api & service list
 gcloud services list --format='csv(TITLE,NAME)' \
 | awk 'NR>=2 {print}' \
 | sort \
 | awk -F, '{printf("# %s\n%s %s %s\n",$1,"gcloud services disable",$2,"--force")}' \
-> disable-services.sh
-chmod +x disable-services.sh
-./disable-services.sh
+> /tmp/disable-services-admin.sh
 
-# Service Usage API
+
+# api & service disable
+bash -v /tmp/disable-services-admin.sh
+
+
+# api & service enable
 gcloud services enable serviceusage.googleapis.com
+
 
 # create bucket
 gcloud storage rm -r gs://admin.chottodake.dev
+
 gcloud storage buckets create gs://admin.chottodake.dev \
 --default-storage-class="STANDARD" \
 --location="ASIA-NORTHEAST2" \
